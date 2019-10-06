4c20871d0825771e8cc9d13d72e34cc24383dc3c
Irina Smirnova <kraizette@gmail.com>
2019-10-07 02:29:03 +0500

    туц

--- LR2/main.cpp
+++ LR2/main.cpp
@@ -7,20 +7,20 @@ int main()
   RCC::AHB1ENR::GPIOAEN::Enable::Set() ;
   GPIOA::MODER::MODER5::Output::Set() ;
   
-  RCC::AHB1ENR::GPIOCEN::Value1::Set() ; //Value1=Enable
-  GPIOC::MODER::MODER9::Value1::Set() ; //Value1=Output;
-  GPIOC::MODER::MODER8::Value1::Set() ;
-  GPIOC::MODER::MODER5::Value1::Set() ;
+  RCC::AHB1ENR::GPIOCEN::Enable::Set() ; //Value1=Enable
+  GPIOC::MODER::MODER9::Output::Set() ; //Value1=Output;
+  GPIOC::MODER::MODER8::Output::Set() ;
+  GPIOC::MODER::MODER5::Output::Set() ;
 
   
   GPIOA::ODR::ODR5::Enable::Set() ;
-  GPIOC::ODR::ODR9::Value1::Set() ;
-  GPIOC::ODR::ODR8::Value1::Set() ;
-  GPIOC::ODR::ODR5::Value1::Set() ;
+  GPIOC::ODR::ODR9::Enable::Set() ;
+  GPIOC::ODR::ODR8::Enable::Set() ;
+  GPIOC::ODR::ODR5::Enable::Set() ;
   
   GPIOA::ODR::ODR5::Disable::Set() ; 
-  GPIOC::ODR::ODR9::Value0::Set() ; //Value1=Disable
-  GPIOC::ODR::ODR8::Value0::Set() ;
-  GPIOC::ODR::ODR5::Value0::Set() ;
-  return 0;
+  GPIOC::ODR::ODR9::Disable::Set() ; //Value1=Disable
+  GPIOC::ODR::ODR8::Disable::Set() ;
+  GPIOC::ODR::ODR5::Disable::Set() ;
+  return 0; 
 }
